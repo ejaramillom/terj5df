bb49d5dcf945f0cdd2f03a7f7daf110dcdde1044
ejaramillom <ejaramilo@hotmail.com>
2020-01-29 20:41:13 -0500

    updated functional app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { firstName: '',
+                   lastName: '',
+                   newUser: '',
+                   users: []
+                 };
+
+    this.handleFirstChange = this.handleFirstChange.bind(this);
+    this.handleLastChange = this.handleLastChange.bind(this);
+    this.addUser = this.addUser.bind(this);
+  }
+  handleFirstChange(event) {
+    this.setState({
+      firstName: event.target.value,
+    });
+  }
+  handleLastChange(event) {
+    this.setState({
+      lastName: event.target.value,
+    });
+  }
+  addUser(event) {
+    event.preventDefault()
+    let oldUsers = this.state.users
+    let newUser = {
+        first: this.state.firstName,
+        last: this.state.lastName,
+      }
+      this.setState({
+        users: [...oldUsers, newUser],
+        newUser: '',
+        firstName: '',
+        lastName: ''
+      })
+    }
+  renderUsers() {
+    const users = this.state.users.map((user, index) => {
+      return (
+        <tr>
+          <td>{user.first}</td>
+          <td>{user.last}</td>
+        </tr>
+      )
+    })
+    return users
+  }
+
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-sm-offset-3">
-            <form>
+            <form onSubmit={this.addUser.bind(this)}>
               <div className="form-group">
                 <label htmlFor="first-name">Nombre</label>
-                <input type="text" className="form-control" name="first-name" />
+                <input type="text" className="form-control" name="first-name" value={this.state.firstName} onChange={this.handleFirstChange}/>
               </div>
 
               <div className="form-group">
                 <label htmlFor="last-name">Apellido</label>
-                <input type="text" className="form-control" name="last-name" />
+                <input type="text" className="form-control" name="last-name" value={this.state.lastName} onChange={this.handleLastChange}/>
               </div>
 
               <div className="action">
-                <button type="submit" className="btn btn-primary">Agregar Invitado</button>
+                <button type="submit" className="btn btn-primary" >Agregar Invitado</button>
               </div>
             </form>
 
@@ -30,7 +78,7 @@ class App extends Component {
                 </tr>
               </thead>
               <tbody>
-
+                {this.renderUsers()}
               </tbody>
             </table>
           </div>
@@ -41,5 +89,3 @@ class App extends Component {
 }
 
 export default App
-
-
